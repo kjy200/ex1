30f13f653cbe2613c7d21aefa3086cd441fe5307
[kjy200] <[rlawnduf200@naver.com]>
2020-03-02 00:56:24 +0900

    www

---
+++ src/main/java/com/ex1/springboot/web/HelloController.java
@@ -0,0 +1,14 @@
+package com.ex1.springboot.web;
+
+import com.ex1.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
+}
